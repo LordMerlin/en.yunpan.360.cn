d2403940200966e0716c81e3a0a15112244da7a7
LordMerlin <ekun@mail.ru>
2014-07-17 18:34:58 +0400

    First commit

---
+++ en.yunpan.360.cn.meta.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @id          yunpan.360.cn
+// @name        yunpan.360.cn
+// @version     0.0.5
+// @namespace   https://github.com/LordMerlin/en.yunpan.360.cn/
+// @updateURL   https://raw.github.com/LordMerlin/en.yunpan.360.cn/master/en.yunpan.360.cn.meta.js
+// @downloadURL https://raw.github.com/LordMerlin/en.yunpan.360.cn/master/en.yunpan.360.cn.user.js
+// @description English translation of the site yunpan.360.cn
+// @include     http://yunpan.360.cn/*
+// @include     http://*.yunpan.360.cn/*
+// @include     http://yunpan.cn/*
+// @include     http://*.yunpan.cn/*
+// @match       http://yunpan.360.cn/*
+// @match       http://*.yunpan.360.cn/*
+// @match       http://yunpan.cn/*
+// @match       http://*.yunpan.cn/*
+// @grant       none
+// ==/UserScript==
